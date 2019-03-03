9ca30dc9a77fe9645a7df51635a0d17a7289bf27
xinyao1220 <47285199+xinyao1220@users.noreply.github.com>
2019-03-02 22:02:14 -0800

    Create concentric-circles.js

---
+++ concentric-circles.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  for (var i = 10; i <= 390; i = i + 10) {
+    ellipse(width/2, height/2, 10 + (390 - i),10 + (390 - i));
+  }
+}
